21a8b52d665993cad4bc4c015828d6b4c954b812
Vojin Golubovic <36790210+fl1k@users.noreply.github.com>
2018-06-01 15:43:07 +0200

    Uploaded

---
+++ tic tac toe/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(28, 21);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(80, 80);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(114, 21);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(80, 80);
+            this.A2.TabIndex = 1;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(200, 21);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(80, 80);
+            this.A3.TabIndex = 2;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(200, 107);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(80, 80);
+            this.B3.TabIndex = 5;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(114, 107);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(80, 80);
+            this.B2.TabIndex = 4;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(28, 107);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(80, 80);
+            this.B1.TabIndex = 3;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(200, 193);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(80, 80);
+            this.C3.TabIndex = 8;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(114, 193);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(80, 80);
+            this.C2.TabIndex = 7;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(28, 193);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(80, 80);
+            this.C1.TabIndex = 6;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(286, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "O:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Turn: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 306);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ tic tac toe/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            turnCheck();
+        }
+
+        bool players = true;
+        int player1 = 0, player2 = 0, draw = 0;
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button playerbutton = (Button)sender;
+            if (playerbutton.Text == "")
+            {
+                if (players == true)
+                    playerbutton.Text = "X";
+                else
+                    playerbutton.Text = "O";
+
+                draw++;
+                if (winCheck() == true)
+                {
+                    if (playerbutton.Text == "X")
+                    {
+                        player1++;
+                        MessageBox.Show("X Won.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        label1.Text = $"X: {player1}";
+                    }
+                    else
+                    {
+                        player2++;
+                        MessageBox.Show("O Won.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        label2.Text = $"O: {player2}";
+                    }
+                    newGame();
+                    draw = 0;
+                }
+
+                if (draw == 9)
+                {
+                    MessageBox.Show("Draw.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    newGame();
+                }
+                players = !players;
+                turnCheck();
+            }
+        }
+
+        private void newGame()
+        {
+            draw = 0;
+            A1.Text = A2.Text = A3.Text = B1.Text = B2.Text = B3.Text = C1.Text = C2.Text = C3.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {        
+            label1.Text = "X: ";
+            label2.Text = "O: ";
+            players = true;
+            newGame();
+            turnCheck();
+        }
+
+        private void turnCheck()
+        {
+            if (players == true)
+                label3.Text = $"Turn: X";
+            else if (players == false)
+                label3.Text = $"Turn: O";
+        }
+
+        private bool winCheck()
+        {
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && A1.Text != "")
+                return true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && B1.Text != "")
+                return true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && C1.Text != "")
+                return true;
+
+            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && A1.Text != "")
+                return true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && A2.Text != "")
+                return true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && A3.Text != "")
+                return true;
+
+            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && A1.Text != "")
+                return true;
+            else if ((C1.Text == B2.Text) && (B2.Text == A3.Text) && A3.Text != "")
+                return true;
+            else
+                return false;
+        }
+    }
+}
